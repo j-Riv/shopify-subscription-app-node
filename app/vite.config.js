772721f3cd34f576a8bcaaaaa64beb801b543617
j-Riv <jose.alfredo.rivera@gmail.com>
2022-07-06 20:52:55 -0700

    Server

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,26 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import 'dotenv/config';
 
 /**
  * @type {import('vite').UserConfig}
  */
-export default {
+// export default {
+//   define: {
+//     'process.env.SHOPIFY_API_KEY': JSON.stringify(process.env.SHOPIFY_API_KEY),
+//   },
+//   plugins: [react()],
+// };
+
+export default defineConfig({
   define: {
     'process.env.SHOPIFY_API_KEY': JSON.stringify(process.env.SHOPIFY_API_KEY),
   },
   plugins: [react()],
-};
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true,
+    },
+  },
+});
