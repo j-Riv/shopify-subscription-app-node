f7a27f037050a8e85530e063be8cd92d65f6bc45
j-Riv <jose.alfredo.rivera@gmail.com>
2023-07-06 11:12:24 -0700

    Updated API Version

--- app/src/server/index.ts
+++ app/src/server/index.ts
@@ -43,7 +43,8 @@ Shopify.Context.initialize({
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
   SCOPES: process.env.SCOPES.split(','),
   HOST_NAME: process.env.HOST.replace(/https:\/\//, ''),
-  API_VERSION: ApiVersion.July22,
+  // @ts-ignore
+  API_VERSION: '2023-07', // ApiVersion.July22,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
   // SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
